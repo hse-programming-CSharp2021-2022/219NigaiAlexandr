b8eedceaa603257bcdcf5ebc5adceeb597446509
ginalgo <aanigay@edu.hse.ru>
2021-09-14 19:18:55 +0300

    sem3 task_02

---
+++ Module_01/Seminar_03/Task_01/Task_02/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Task_02
+{
+    class Program
+    {
+        static void ForLoop()
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine(i * i);
+            }
+        }
+
+        static void WhileLoop()
+        {
+            int i = 1;
+            while (i <= 10)
+            {
+                Console.WriteLine(i * i);
+                i++;
+            }
+        }
+
+        static void DoLoop()
+        {
+            int i = 1;
+            do
+            {
+                Console.WriteLine(i * i);
+                i++;
+            } while (i <= 10);
+        }
+
+        static void Main(string[] args)
+        {
+            ForLoop();
+            WhileLoop();
+            DoLoop();
+        }
+    }
+}
