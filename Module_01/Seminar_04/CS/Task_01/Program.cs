d9e58f880b1e0af4fc0e1548e25c23bd0c9425b2
ginalgo <aanigay@edu.hse.ru>
2021-09-25 22:59:44 +0300

    Seminar 4 CS

---
+++ Module_01/Seminar_04/CS/Task_01/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Task_01
+{
+    class Program
+    {
+        static bool Transform(ref uint numb)
+        {
+            if (numb.ToString().Length != 3) return false;
+            var digits = new List<uint>();
+            while (numb > 0)
+            {
+                digits.Add(numb % 10);
+                numb /= 10;
+            }
+            digits.Sort();
+            numb = digits[2] * 100 + digits[1] * 10 + digits[0];
+            return true;
+        }
+        
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                uint a = uint.Parse(Console.ReadLine());
+                Transform(ref a);
+                Console.WriteLine(a);
+            }
+        }
+    }
+}
---
+++ Module_01/Seminar_04/CS/Task_03/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Task_03
+{
+    class Program
+    {
+
+        static bool Triangle(double x, double y, double z, out double p, out double s)
+        {
+            if (x + y < z || y + z < x || z + x < y)
+            {
+                s = 0;
+                p = 0;
+                return false;
+            }
+            p = x + y + z;
+            s = Math.Sqrt((p / 2) * (p / 2 - x) * (p / 2 - y) * (p / 2 - z));
+            return true;
+        }
+        
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Write("Введи сторону x: ");
+                double x = double.Parse(Console.ReadLine());
+                Console.Write("Введи сторону y: ");
+                double y = double.Parse(Console.ReadLine());
+                Console.Write("Введи сторону z: ");
+                double z = double.Parse(Console.ReadLine());
+
+                Triangle(x, y, z, out double p, out double s);
+                Console.WriteLine($"Периметр: {p}\nПлощадь: {s}");
+            }
+        }
+    }
+}
