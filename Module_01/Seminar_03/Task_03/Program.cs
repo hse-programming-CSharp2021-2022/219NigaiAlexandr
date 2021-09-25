122201d246f97739c271d85bc1dc124446c4ab44
ginalgo <aanigay@edu.hse.ru>
2021-09-25 23:22:06 +0300

    CS 3

---
+++ Module_01/Seminar_03/Task_03/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Task_03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            double a = 10, delta = 0.02, k = 0, s = 0;
+            while (k < a)
+            {
+                s += ((k * k) + (k + delta) * (k + delta)) * delta / 2;
+                k += delta;
+            }
+
+            Console.WriteLine(s);
+        }
+    }
+}
